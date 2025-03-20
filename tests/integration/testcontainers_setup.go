e3c8d3761198472639df6e46420e664804befdf3
Ajeet Singh Raina, Docker Captain, ARM Innovator <ajeetraina@gmail.com>
2025-03-20 22:57:56 +0530

    Add placeholder for testcontainers_setup.go

---
+++ tests/integration/testcontainers_setup.go
@@ -0,0 +1,23 @@
+// This file is commented out temporarily to make the tests compile
+// It will be fully implemented once the dependencies are properly set up
+
+/*
+package integration
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/docker/go-connections/nat"
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+// ... Full implementation will be added later ...
+*/
+
+package integration
